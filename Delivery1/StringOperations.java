37431c8db1aa6c103242cdf43c9416aae537c7d5
Yousef-nazeer <66140196+Yousef-nazeer@users.noreply.github.com>
2023-03-22 14:49:04 +0200

    Add files via upload

---
+++ Delivery1/StringOperations.java
@@ -0,0 +1,8 @@
+public class MyClass {
+    public static void main(String args[]) {
+     String text = "Hello";
+    for (int i = 0; i <text.length() ; i++) {
+  System.out.println(text.substring(i));
+}
+    }
+}

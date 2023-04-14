a02d9e5b3ee5a6902b382f080559adf3192b5d31
arsanious isaac <65775188+arsanious-isaac@users.noreply.github.com>
2023-04-14 22:48:04 +0200

    Update DynamicCodeAnalyzer.java

--- src/DynamicCodeAnalyzer.java
+++ src/DynamicCodeAnalyzer.java
@@ -94,7 +94,7 @@ public class DynamicCodeAnalyzer extends JavaParserBaseListener {
             rewriter2.insertAfter(ctx.statement(0).getStop(), "}");
         }
         else rewriter.insertAfter(ctx.statement(0).getStart(), "\n\t\t" + build.toString() + "\n\t\t");
-        if(ctx.getChild(4).getText().charAt(0) != '{') {
+        if( ctx.getChild(4) != null && ctx.getChild(4).getText().charAt(0) != '{') {
             counter++;
             rewriter.insertBefore(ctx.statement(1).getStart(),
                     "{//block " + (counter) + "\n\t\t  blocks.println(\"block #" + counter + " is visited\");\n");

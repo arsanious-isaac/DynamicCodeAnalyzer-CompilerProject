7aefa1e543ad1cbfb9543953df433ea8ad53cd2b
arsanious isaac <65775188+arsanious-isaac@users.noreply.github.com>
2023-04-14 22:50:01 +0200

    Delete StringOperations.java

--- Delivery1/StringOperations.java
+++
@@ -1,8 +0,0 @@
-public class MyClass {
-    public static void main(String args[]) {
-     String text = "Hello";
-    for (int i = 0; i <text.length() ; i++) {
-  System.out.println(text.substring(i));
-}
-    }
-}
